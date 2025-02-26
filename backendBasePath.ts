0a386ef9f3a114f18e1ff02093feeb29ee423a72
Andrey Soares <andreysoares.dev@gmail.com>
2025-02-25 23:40:11 -0300

    chore: update backend endpoint + remove unused deps

--- backendBasePath.ts
+++ backendBasePath.ts
@@ -1,6 +1,3 @@
-const backendBasePath =
-  process.env.NODE_ENV === "production"
-    ? "http://34.23.51.63/api/:path*"
-    : "http://127.0.0.1:8000/api/:path*";
+const backendBasePath = "http://34.23.51.63/api/:path*";
 
 export default backendBasePath;
--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ import pkg from "webpack";
 
 const { ProvidePlugin } = pkg;
 
-const backendBasePath =
-  process.env.NODE_ENV === "production"
-    ? "http://34.23.51.63/api/:path*"
-    : "http://127.0.0.1:8000/api/:path*";
+const backendBasePath = "http://34.23.51.63/api/:path*";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
